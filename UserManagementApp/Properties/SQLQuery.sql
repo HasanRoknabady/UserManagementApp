db1f3298be38a5937d0a1e7ea8596e8151c1e236
Hasan Roknabady <78913791+HasanRoknabady@users.noreply.github.com>
2024-06-14 15:57:13 +0330

    Update SQLQuery.sql

--- UserManagementApp/Properties/SQLQuery.sql
+++ UserManagementApp/Properties/SQLQuery.sql
@@ -1,7 +1,3 @@
-﻿-- Drop the Users table if it exists (be careful with this as it will delete all existing data)
--- DROP TABLE IF EXISTS [dbo].[Users];
-
--- Create the Users table
 CREATE TABLE [dbo].[Users] (
     [Id]       INT           IDENTITY (1, 1) NOT NULL,
     [Username] NVARCHAR (50) NOT NULL,
@@ -11,6 +7,6 @@ CREATE TABLE [dbo].[Users] (
     UNIQUE NONCLUSTERED ([Username] ASC)
 );
 
--- If the table already exists, just update the Password column
+
 ALTER TABLE [dbo].[Users]
 ALTER COLUMN [Password] NVARCHAR(64) NOT NULL;
